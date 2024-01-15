549d71b7551266ab7b1419aa8906ed7c22dbfb92
Alexander Karlberg <alexander.karlberg@cern.ch>
2024-01-15 12:52:54 +0100

    Ported change in hoppet for where xmur and xmuf are passed

--- src/disorder.f90
+++ src/disorder.f90
@@ -205,18 +205,17 @@ contains
    call hoppetStartExtended(ymax_hoppet,dy,minQval,maxQval,dlnlnQ,nloop,&
         &         order_hoppet,factscheme_MSbar)
    if(vnf) then
-      call StartStrFct(order_max = order_max, xR = xmur, xF = xmuf,&
-           & scale_choice = scale_choice_hoppet, constant_mu = mz,&
-           & param_coefs = .true. , wmass = mw,&
-           & zmass = mz)
+      call StartStrFct(order_max = order_max, scale_choice =&
+           & scale_choice_hoppet, constant_mu = mz, param_coefs =&
+           & .true. , wmass = mw, zmass = mz)
    else
-      call StartStrFct(order_max = order_max, nflav = nflav, xR =&
-           & xmur, xF = xmuf, scale_choice = scale_choice_hoppet,&
-           & constant_mu = mz, param_coefs = .true., &
-           & wmass = mw, zmass = mz)      
+      call StartStrFct(order_max = order_max, nflav = nflav,&
+           & scale_choice = scale_choice_hoppet, constant_mu = mz,&
+           & param_coefs = .true., wmass = mw, zmass = mz)      
    endif
    call read_PDF()
-   call InitStrFct(order_max, separate_orders = separate_orders)
+   call InitStrFct(order_max, separate_orders = separate_orders, xR =&
+        & xmur, xF = xmuf)
 
    if(novegas) then ! This means Q and x fixed
       ! Need dummy random numbers
